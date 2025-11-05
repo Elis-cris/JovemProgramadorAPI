17386f060f523ebff3c9f7a721c78a1d02bce17d
Frank17128 <cicinho171288@outlook.com>
2025-11-04 21:23:14 -0300

    Adicionar arquivos de projeto.

---
+++ JovemProgramadorAPI/Controllers/AlunoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Modelo.Domain;
+
+namespace JovemProgramadorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public AlunoController()
+        {
+                
+        }
+
+        [HttpGet("BuscarDadosAluno/{Id}")]
+
+        public async Task<IActionResult> BuscarDadosAlunos(int Id)
+        {
+            try
+            {
+                Aluno aluno = new Aluno();
+
+                aluno.Id = Id;
+                aluno.Nome = "Francisco";
+                aluno.Matricula = "ABC123";
+
+                return Ok(aluno);
+
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+        }
+
+    }
+}
---
+++ Modelo_Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Modelo_Application
+{
+    public class Class1
+    {
+
+    }
+}
